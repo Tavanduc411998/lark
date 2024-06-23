0e8a68faff4df9b228b7c463440ef8058173083c
Tavanduc411998 <tavanduc2018@example.com>
2024-06-23 08:06:23 +0700

    update header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./component/NavBar";
+import Download from "./download/Download";
 
 function App() {
-  return <NavBar></NavBar>;
+  return (
+    <Fragment>
+      <NavBar></NavBar>
+      <Download></Download>
+    </Fragment>
+  );
 }
 
 export default App;
--- src/download/Download.tsx
+++ src/download/Download.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Download.scss";
 
 const Download = () => {
   return (
-    <div>Download</div>
-  )
-}
+    <div className="download-container">
+      <div className="header">
+        <div>
+          <h2>Lark for Windows</h2>
+          <p>Start your journey to productivity today. Try Lark for free.s</p>
+          <button className="download-btn">
+            <i className="fa-solid fa-download"></i>
+            Download for PC
+          </button>
+        </div>
+      </div>
+      <div className="body"></div>
+    </div>
+  );
+};
 
-export default Download
+export default Download;
